1036fcae32cdcf8afd02a411ba35f31c8dd436f1
Alireza Mohebbi <aasoftmohebbi@gmail.com>
2025-01-03 15:47:25 +0330

    start

---
+++ code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void){
+string name="";
+printf("hello what is your name? \n\r");
+name=scanf("%s",&name);
+if(name){
+printf("hello %s",name);
+}
+
+}
